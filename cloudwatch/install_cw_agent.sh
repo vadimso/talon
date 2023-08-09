f84661b1eb6c971ef81c66236720ed4adf47a929
Vadim Sohin , DevOps  Engineer <vadim.sohin@gmail.com>
2023-08-09 11:43:49 +0200

    Create install_cw_agent.sh

---
+++ cloudwatch/install_cw_agent.sh
@@ -0,0 +1,12 @@
+# SSH into one of  EKS nodes
+ssh -i <path_to_ssh_key> ec2-user@<node_public_ip>
+
+# Download and install the CloudWatch agent
+wget https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
+sudo rpm -U ./amazon-cloudwatch-agent.rpm
+
+# Configure the CloudWatch Agent
+/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json -s
+
+# open aws console and verify that logs are sent 
